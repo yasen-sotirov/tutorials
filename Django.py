bf64a2a9c7d4ceeac472016985c462304c8a5220
Yasen Sotirov <ia.sotirov@gmail.com>
2024-04-02 10:16:03 +0300

    test

---
+++ Django.py
@@ -0,0 +1,19 @@
+"DJANGO"   #
+
+
+"""
+ИНСТАЛИРАНЕ ВИРТУАЛНА СРЕДА
+    pip install virtualenv env
+
+
+АКТИВИРАНЕ VENV
+    CMD в лентата с локацията на файл
+    env\scripts\ activate (deactivate)
+
+СТАРТИЕАНЕ СЪРВЪР
+    \Django\studybud\python manage.py runserver
+    
+    
+    
+    
+"""
---
+++ Django/studybud/base/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+rooms = [
+    {'id':1, 'name':'let leatn Python!'},
+    {'id':2, 'name':'Design with me'},
+    {'id':3, 'name':'Frontend developers'},
+]
+
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def room(request):
+    return render(request,'room.html')
+
