29b66105cf3e1a3556206f20c8c546c819d344c6
Yasen Sotirov <ia.sotirov@gmail.com>
2024-04-09 10:13:26 +0300

    tasks

--- JS/11_tic_tack_toe/index.js
+++ JS/11_tic_tack_toe/index.js
@@ -14,8 +14,41 @@ function clearBoard() {
 restart.addEventListener('click', clearBoard);
 
 
+
 // check square markers
 
+// ВАРИАНТ 1
+// var cellOne = document.querySelector('#one')
+// 
+// cellOne.addEventListener('click', function() {
+//     if (cellOne.textContent === '') {
+//         cellOne.textContent = 'X';
+//     }
+//     else if (cellOne.textContent === 'X') {
+//         cellOne.textContent = 'O';
+//     }
+//     else  {
+//         cellOne.textContent = '';}
+// })
+
+
+// ВАРИАНТ 2 - THIS
+function changeMarker() {
+    if (this.textContent === ''){
+        this.textContent = 'X';
+    }
+    else if (this.textContent === 'X'){
+        this.textContent = 'O';
+    }
+    else {
+        this.textContent = '';
+    }
+}
+
+
+for (var i = 0; i < squares.length; i++) {
+    squares[i].addEventListener('click', changeMarker)
+}
 
 
 
